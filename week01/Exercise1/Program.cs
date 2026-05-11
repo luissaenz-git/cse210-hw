0833849e35487cae706024d6ef8d4a76c75fca38
personal <lsaenz@byupathway.edu>
2026-05-10 22:14:52 -0400

    Exercise 1 concluded

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string firstname = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastname = Console.ReadLine();
+        Console.WriteLine("");
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
     }
 }
